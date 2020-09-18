5ea95090646bb02fee0781ae469634c07f947d8c
Erlind333 <70763816+Erlind333@users.noreply.github.com>
2020-09-18 15:18:18 +0200

    Add files via upload

---
+++ Isaa/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <met description="Website makinash">
+
+    <title>my first website after COVID-19</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">My website</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Page two</a></li>
+          <li><a href="#">Page three</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <!-- Lista e fruta perimeve -->
+        <div class="col-xs-12 col-md-6">
+          <ol>
+            <li>1 kg kumblla</li>
+            <li>1 shalqin</li>
+            <li>5 banane "Bana King"</li>
+          </ol>
+        </div>
+        <!-- Lista e gjerave per tualet -->
+        <div class="col-xs-12 col-md-6">
+          <ul class="listaTualet">
+            <li>Leter higjenike</li>
+            <li>3 Sapuna</li>
+            <li>Ilac robash</li>
+            <li class="trt">Lenor</li>
+          </ul>
+        </div>
+      </div><!-- End row -->
+      <p id="NgjyreBlu" class="tekstiKuq meTeMedha">Some content</p>
+      <button id="testing" class="btn btn-success" onclick="showData()">Shfaq Daten edhe oren</button>
+      <p id="mesazhi"></p>
+
+<button id="BtnNgjyre" onclick="ngjyrostekstin()">Jep Ngjyren Blue</button>
+
+<!---Detyre Shtepie 1 -->
+
+<button id="Detyre" class="btn btnsuccess">Llogarit</button>
+
+    </div><!-- End container -->
+  </body>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+  <noscript>
+    Sorry, you need to activate Javascript!
+  </noscript>
+</html>
---
+++ Isaa/js/main.js
@@ -0,0 +1,38 @@
+
+var test = "Jeni duke studiuar javascript";
+var extension = "Shtese Stingesh";
+
+var problem_result = "Rezultati";
+
+var a = 3;
+var b = 4;
+
+ //test, extension etj etj
+//Concatenation
+console.log(test +' '+extension+' kot');
+console.log(a+''+b);
+console.log(problem_result+': '+(a+b));
+
+
+
+function showData() {
+  document.getElementById('mesazhi').innerHTML = Date();
+  //$('#mesazhi')
+}
+
+function ngjyrostekstin() {
+  document.getElementById('NgjyreBlu').style.color = "#00f"
+}
+
+
+//Detyra e Shtepise Nr 1
+
+$('#Detyre').click(function(){
+  alert("Urime Ju Moret Noten 10");
+});
+var a=4
+var b=6
+var c=a+b;
+console.log("Urime Ju Moret Noten 10");
+console.log(a+b);
+console.log(a+''+b)
